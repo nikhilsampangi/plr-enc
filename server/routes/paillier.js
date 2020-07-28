94b4c751b9e9729af1d2ab7762bfb6b16d5ab696
nikhilsampangi <nikhil.sampangi@gmail.com>
2020-07-28 18:00:46 +0530

    added routes for the 3 hmmorphic properties

--- server/routes/paillier.js
+++ server/routes/paillier.js
@@ -19,8 +19,14 @@ const { spawn } = require("child_process");
 //   });
 // }
 
+// 1.Algorithm
+
 // key generation
 router.get("/generate_keys", key_gen);
+// encryption
+router.post("/encrypt", encrypt);
+// decryption
+router.post("/decrypt", decrypt);
 
 function key_gen(req, res) {
   var process = spawn("py", ["./python/keygen.py"]);
@@ -35,11 +41,8 @@ function key_gen(req, res) {
   });
 }
 
-// encryption
-router.post("/encrypt", encrypt);
-
 function encrypt(req, res) {
-  console.log("ip check: ", req.body);
+  // console.log("ip check: ", req.body);
   var process = spawn("py", ["./python/encrypt.py", req.body.pub, req.body.x]);
   process.stdout.on("data", (data) => {
     var cipher_str = data.toString().split(/(\r?\n)/g);
@@ -50,9 +53,6 @@ function encrypt(req, res) {
     res.status(400).send("encrypt spawn error");
   });
 }
-
-// decryption
-router.post("/decrypt", decrypt);
 
 function decrypt(req, res) {
   var process = spawn("py", [
@@ -68,6 +68,69 @@ function decrypt(req, res) {
   process.stderr.on("data", (data) => {
     console.log(`decrypt spawn error:${data}`);
     res.status(400).send("decrypt spawn error");
+  });
+}
+
+// 2.Homomorphic properties
+
+// adding two ciphers
+router.post("/add_ciphers", cipher_add);
+// adding constant to cipher
+router.post("/add_constant", const_add);
+// multiplying constant to cipher
+router.post("/mult_const", const_mult);
+
+function cipher_add(req, res) {
+  var process = spawn("py", [
+    "./python/compute.py",
+    "1",
+    req.body.pub,
+    req.body.x,
+    req.body.y,
+  ]);
+  process.stdout.on("data", (data) => {
+    var sol_str = data.toString().split(/(\r?\n)/g);
+    res.send({ soln: sol_str[0] });
+  });
+  process.stderr.on("data", (data) => {
+    console.log(`e_add spawn error:${data}`);
+    res.status(400).send("e_add spawn error");
+  });
+}
+
+function const_add(req, res) {
+  var process = spawn("py", [
+    "./python/compute.py",
+    "2",
+    req.body.pub,
+    req.body.x,
+    req.body.const,
+  ]);
+  process.stdout.on("data", (data) => {
+    var sol_str = data.toString().split(/(\r?\n)/g);
+    res.send({ soln: sol_str[0] });
+  });
+  process.stderr.on("data", (data) => {
+    console.log(`add spawn error:${data}`);
+    res.status(400).send("add spawn error");
+  });
+}
+
+function const_mult(req, res) {
+  var process = spawn("py", [
+    "./python/compute.py",
+    "3",
+    req.body.pub,
+    req.body.x,
+    req.body.const,
+  ]);
+  process.stdout.on("data", (data) => {
+    var sol_str = data.toString().split(/(\r?\n)/g);
+    res.send({ soln: sol_str[0] });
+  });
+  process.stderr.on("data", (data) => {
+    console.log(`mult spawn error:${data}`);
+    res.status(400).send("mult spawn error");
   });
 }
 
